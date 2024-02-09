d5ea95f6cfbf35da33c744f9c3e5a78d8912906c
Daewook Lee <91823083+LeeDaeWook@users.noreply.github.com>
2024-02-09 21:09:35 +0900

    [Gold V] Title: 괄호의 값, Time: 204 ms, Memory: 17696 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.java
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.java
@@ -8,11 +8,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
-        if (input.length() % 2 == 1) {
-            System.out.println(0);
-            return ;
-        }
-
         // 입력된 string을 정수형으로 변환
         int[] arr = new int[input.length()];
         for (int i = 0; i < input.length(); i++) {
