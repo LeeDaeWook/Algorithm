8f7dd501b1865badf4de5a1eb3e2e3c01d5cff45
Daewook Lee <91823083+LeeDaeWook@users.noreply.github.com>
2025-03-01 05:50:46 -0800

    [level 2] Title: [3차] n진수 게임, Time: 97.08 ms, Memory: 104 MB -BaekjoonHub

---
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    String s = "0123456789ABCDEF";
+    
+    public String solution(int n, int t, int m, int p) {
+        StringBuilder answer = new StringBuilder();
+        Deque<Character> nums = new ArrayDeque<>();
+        
+        for (int i = t*m - 1; i >= 0; i--) {
+            int num = i;
+            while (num > 0) {
+                nums.addFirst(s.toCharArray()[num%n]);
+                num /= n;
+            }
+        }    
+        nums.addFirst('0');
+                
+        Character[] numsArray = nums.stream().map(Character::charValue).toArray(Character[]::new);
+        
+        for (int i = 0; answer.length() < t; i += m)
+            answer.append(numsArray[p-1 + i]);
+        
+        return answer.toString();
+    }    
+}
